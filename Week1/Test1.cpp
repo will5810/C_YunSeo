53e2b98580d9f8c1ba824f5948954456c8a174f3
will5810 <79901583+will5810@users.noreply.github.com>
2021-03-02 17:32:21 +0900

    Add files via upload

---
+++ Week1/Test1.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+main()
+{
+/*
+	printf("Hungry Morning.\n");
+	
+	printf("안녕하세요. Hi");
+	printf("Hello world!");
+
+   printf("%d %d\n",1234,12);
+   printf("내 이름은 %s 이구요\n\n","박윤서");
+   printf("제 나이는 %d 입니다.\n\\a: 경고음 발생.",25); 
+   
+   
+   printf("%d%d",123,456);//123456
+   //123_____456_____789 출력되도록 하세요 
+  
+   
+   printf("%8d %8d %8d\n",123,456,789);
+   
+   printf("실수의 출력 : %8.2f %10.3f",3.14159265,456.789123);
+*/  
+     
+   printf("data type 별 byte 수 (char) : %d byte\n",sizeof(char));
+   printf("data type 별 byte 수 (int) : %d byte\n",sizeof(int));
+   printf("data type 별 byte 수 (long) : %d byte\n",sizeof(long));
+   printf("data type 별 byte 수 (float) : %d byte\n",sizeof(float));
+   printf("data type 별 byte 수 (double) : %d byte\n",sizeof(double));
+}
+
